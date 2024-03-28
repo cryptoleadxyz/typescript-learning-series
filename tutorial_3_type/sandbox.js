5346040cdeddf4d604b6a0aaed88fbde531d5c05
cryptoleadxyz <cryptolead.eth@gmail.com>
2024-03-27 21:11:26 -0600

    add tutorial #3 content

---
+++ tutorial_3_type/sandbox.js
@@ -0,0 +1,7 @@
+var character = "goku";
+var age = 30;
+var isBlackBelt = false;
+var circ = function (diameter) {
+    return Math.PI * diameter;
+};
+console.log(circ(6564));
---
+++ tutorial_3_type/sandbox.ts
@@ -0,0 +1,9 @@
+let character = "goku";
+let age = 30;
+let isBlackBelt = false;
+
+const circ = (diameter: number) => {
+  return Math.PI * diameter;
+};
+
+console.log(circ(6564));
